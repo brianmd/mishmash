3a78db06ed5dab82dd617e1794bd5ace05a50dd4
Brian Murphy-Dye <brian@murphydye.com>
2017-03-05 16:47:14 -0700

    Update version to 0.1.1

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject com.murphydye/mishmash "0.1.0-SNAPSHOT"
+(defproject com.murphydye/mishmash "0.1.1-SNAPSHOT"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
